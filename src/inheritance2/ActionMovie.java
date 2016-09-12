511fb52a2444a8c524fd8e33f0b259e04900f39a
kevinjerke <kevinjerke@172.16.42.12>
2016-09-12 16:38:29 -0500

    Extending classes and adding getter and setter in ActionMovie class

---
+++ src/inheritance2/ActionMovie.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kevinjerke
+ */
+public class ActionMovie {
+    String characterType;
+    
+    public void setCharacterType(String character){
+        characterType = character;
+    }
+    
+    public String getCharacterType(){
+        return characterType;
+    }
+    
+    public void getCharacterDetails(){
+        JOptionPane.showMessageDialog(null, "Character Details: " + getCharacterType());
+    }
+}
